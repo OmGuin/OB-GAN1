d23bc3fc0fdd710dac404745b3e46a7f343af6ad
Om Guin <79595320+OmGuin@users.noreply.github.com>
2023-07-07 14:44:08 +0000

    cheese is sometimes good for you

---
+++ config.py
@@ -0,0 +1,27 @@
+import torch
+
+#los pollos hermanos
+BATCH_SIZE = 8
+RESIZE_TO = 1024 
+NUM_EPOCHS = 15 
+NUM_WORKERS = 4
+
+DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+# training images and XML files directory
+TRAIN_DIR = 'FinalDataset/train'
+# validation images and XML files directory
+VALID_DIR = 'FinalDataset/valid'
+
+# classes: 0 index is reserved forW background
+CLASSES = [
+    '__background__', 'Nodule'
+]
+
+NUM_CLASSES = len(CLASSES)
+
+# whether to visualize images after creating the data loaders
+VISUALIZE_TRANSFORMED_IMAGES = False
+
+# location to save model and plots
+OUT_DIR = 'modelandplots'
